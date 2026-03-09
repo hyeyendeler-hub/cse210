dcc38dbfb6b90b6aeaca2eafb107bfcc8fcc1d36
hyeyendelerashidi <hyeyendeler@gmail.com>
2026-03-09 11:11:01 +0300

    Finished

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your home country?");
+        string homeCountry = Console.ReadLine();
+
+        Console.WriteLine("You are from {0}", homeCountry);
+        string capitalCity = homeCountry.ToUpper(); 
+         
+        Console.WriteLine("In which district do you come from?");
+        string district = Console.ReadLine();   
+
+        Console.WriteLine("You are from {0}", district);
+        string capitalLetters = district.ToUpper();
+
+        Console.WriteLine("The capital of {0} is {1}", homeCountry, capitalCity);
+        Console.WriteLine("The district you come from is {0}", capitalLetters);
+
     }
 }
