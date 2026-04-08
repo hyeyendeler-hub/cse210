52a3486cf2f4c3285a2b586817397e2a5f2d3934
hyeyendelerashidi <hyeyendeler@gmail.com>
2026-04-08 16:53:07 +0300

    finished work

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,60 @@
+using System;
+
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string shortName, string description, int points, int target, int bonus) 
+        : base(shortName, description, points)
+    {
+        _amountCompleted = 0;
+        _target = target;
+        _bonus = bonus;
+    }
+
+    public ChecklistGoal(string shortName, string description, int points, int target, int bonus, int amountCompleted) 
+        : base(shortName, description, points)
+    {
+        _amountCompleted = amountCompleted;
+        _target = target;
+        _bonus = bonus;
+    }
+
+    public override int RecordEvent()
+    {
+        _amountCompleted++;
+        if (_amountCompleted >= _target)
+        {
+            return GetPoints() + _bonus;
+        }
+        return GetPoints();
+    }
+
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    public int GetAmountCompleted()
+    {
+        return _amountCompleted;
+    }
+
+    public int GetTarget()
+    {
+        return _target;
+    }
+
+    public override string GetDetailsString()
+    {
+        string status = IsComplete() ? "[X]" : "[ ]";
+        return $"{status} {GetShortName()} ({GetDescription()}) -- Currently completed: {_amountCompleted}/{_target}";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal|{GetShortName()}|{GetDescription()}|{GetPoints()}|{_target}|{_bonus}|{_amountCompleted}";
+    }
+}
---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,57 @@
+using System;
+
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private int _points;
+
+    public Goal(string shortName, string description, int points)
+    {
+        _shortName = shortName;
+        _description = description;
+        _points = points;
+    }
+
+    public string GetShortName()
+    {
+        return _shortName;
+    }
+
+    public void SetShortName(string shortName)
+    {
+        _shortName = shortName;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+    public void SetPoints(int points)
+    {
+        _points = points;
+    }
+
+    public abstract int RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public abstract string GetStringRepresentation();
+
+    public virtual string GetDetailsString()
+    {
+        string status = IsComplete() ? "[X]" : "[ ]";
+        return $"{status} {GetShortName()} ({GetDescription()})";
+    }
+}
