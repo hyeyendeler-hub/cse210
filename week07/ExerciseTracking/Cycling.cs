1f4b57ebde3f57b1038ceb3f070da1cf3cd40eba
hyeyendelerashidi <hyeyendeler@gmail.com>
2026-04-13 18:30:07 +0300

    all done

--- week07/ExerciseTracking/Cycling.cs
+++ week07/ExerciseTracking/Cycling.cs
@@ -2,25 +2,25 @@ using System;
 
 public class Cycling : Activity
 {
-    private double _distance; // in miles
+    private double _speed; // in mph
 
-    public Cycling(string date, int length, double distance) : base(date, length)
+    public Cycling(string date, int length, double speed) : base(date, length)
     {
-        _distance = distance;
+        _speed = speed;
     }
 
     public override double GetDistance()
     {
-        return _distance;
+        return _speed * (_length / 60.0);
     }
 
     public override double GetSpeed()
     {
-        return _distance / (_length / 60.0);
+        return _speed;
     }
 
     public override double GetPace()
     {
-        return _length / _distance;
+        return _length / GetDistance();
     }
 }
